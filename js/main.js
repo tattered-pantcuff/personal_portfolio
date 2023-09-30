b0fa57c9124c059f305e204c98c0d39c79bc088a
Christina <christina05301@gmail.com>
2023-09-30 09:12:51 -0400

    add event handler function in js file

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const button = document.getElementById("contact-button");
+
+
+button.onclick = (event) => {
+   
+    document.getElementById("contact").innerHTML = "email me at christina05301@gmail.com";
+    
+    
+    
+
+
+}
